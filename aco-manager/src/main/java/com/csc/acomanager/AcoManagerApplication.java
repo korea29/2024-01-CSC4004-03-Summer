fa96a2838b6eeb08a51b4cba882a5f5ca9798058
HakHae <121737437+HakHae@users.noreply.github.com>
2024-05-01 22:13:38 +0900

    Add files via upload
    스프링 부트 데모 코드 업로드

---
+++ aco-manager/src/main/java/com/csc/acomanager/AcoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.csc.acomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcoManagerApplication.class, args);
+	}
+
+}
