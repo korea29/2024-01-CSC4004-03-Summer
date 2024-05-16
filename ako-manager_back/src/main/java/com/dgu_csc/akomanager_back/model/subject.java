1d0276aab17b8e2345f33ef966d96595cec06ace
kimseonpyo <kimsp0317@gmail.com>
2024-05-16 13:38:38 +0900

    모델 / 서비스 / 컨트롤러 작동 완료 확인

--- ako-manager_back/src/main/java/com/dgu_csc/akomanager_back/model/subject.java
+++ ako-manager_back/src/main/java/com/dgu_csc/akomanager_back/model/subject.java
@@ -14,24 +14,31 @@ public class subject {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 학수번호
     @Column(name = "subject_num", nullable = false, length = 8)
     private String subjectNum;
 
+    // 개설학년
     @Column(name = "established_grade", length = 8)
     private String establishedGrade;
 
+    // 개설학기
     @Column(name = "open_semester", length = 8)
     private String openSemester;
 
-    @Column(name = "` course_of_study`", length = 10)
+    // 교과과정
+    @Column(name = "course_of_study", length = 10)
     private String courseOfStudy;
 
+    // 이수구분
     @Column(name = "classification_of_completion", length = 10)
     private String classificationOfCompletion;
 
+    // 교과목명
     @Column(name = "subject_name", length = 100)
     private String subjectName;
 
+    // 학점
     @Column(name = "grade", nullable = false)
     private Integer grade;
 
