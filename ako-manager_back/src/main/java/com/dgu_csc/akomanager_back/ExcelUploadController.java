46dee69df5ca695a3c830e45603583db6cea716a
HakHae <jwn634@gmail.com>
2024-05-16 11:36:31 +0900

    전체 과목 저장을 위한 코드 구현

--- ako-manager_back/src/main/java/com/dgu_csc/akomanager_back/ExcelUploadController.java
+++ ako-manager_back/src/main/java/com/dgu_csc/akomanager_back/ExcelUploadController.java
@@ -5,15 +5,21 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.http.ResponseEntity;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("/excel")
 public class ExcelUploadController {
     private final List<String[][]> excels = new ArrayList<String[][]>(); // 각 액셀 파일을 2차원 스트링 형태로 저장
-    
+
     @PostMapping("/upload")
     public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
         if (file.isEmpty()) {
@@ -29,7 +35,7 @@ public class ExcelUploadController {
         }
     }
 
-    public String[][] readExcel(MultipartFile file) throws Exception {
+    public static String[][] readExcel(MultipartFile file) throws Exception {
         InputStream is = file.getInputStream();
         Workbook workbook = new XSSFWorkbook(is);
         Sheet sheet = workbook.getSheetAt(0);
@@ -46,5 +52,102 @@ public class ExcelUploadController {
         }
         workbook.close();
         return data;
+    }
+
+    public static void readExcel(String path, String filename, ArrayList<Subject> list) {
+        try {
+            FileInputStream file = new FileInputStream(path + filename);
+            Workbook workbook = WorkbookFactory.create(file);
+
+            NumberFormat f = NumberFormat.getInstance();
+            f.setGroupingUsed(false); // 지수로 안나오게 설정
+
+            int sheetNum = workbook.getNumberOfSheets(); // 시트 갯수
+
+            for (int s = 0; s < sheetNum; s++) {
+                Sheet sheet = workbook.getSheetAt(s);
+                int rows = sheet.getPhysicalNumberOfRows();
+
+                for (int r = 1; r < rows; r++) {
+                    Row row = sheet.getRow(r);
+
+                    int cells = row.getPhysicalNumberOfCells();
+
+                    Subject newSubject = new Subject();
+
+                    for (int c = 0; c < cells - 3; c++) {
+                        Cell cell = row.getCell(c);
+
+                        String value = "";
+                        if (cell != null) {
+                            switch (cell.getCellType()) {
+                                case STRING:
+                                    value = cell.getStringCellValue();
+                                    break;
+                                case NUMERIC:
+                                    if (DateUtil.isCellDateFormatted(cell)) {
+                                        value = cell.getDateCellValue().toString();
+                                    } else {
+                                        value = f.format(cell.getNumericCellValue()) + "";
+                                    }
+                                    break;
+                                case BOOLEAN:
+                                    value = cell.getBooleanCellValue() + "";
+                                    break;
+                                case BLANK:
+                                    value = "";
+                                    break;
+                                case ERROR:
+                                    value = cell.getErrorCellValue() + "";
+                                    break;
+                            }
+                        }
+
+                        switch (c) {
+                            case 1: { // 개설 학년
+                                String[] numbers = value.split(",");
+                                ArrayList<String> schoolYears = new ArrayList<>();
+                                Collections.addAll(schoolYears, numbers);
+                                newSubject.setYearOfOffering(schoolYears);
+                                break;
+                            }
+                            case 2: { // 개설 학기
+                                newSubject.setSemesterOfOffering(value);
+                                break;
+                            }
+                            case 3: { // 교과과정
+                                newSubject.setCurriculum(value);
+                                break;
+                            }
+                            case 5: { // 이수구분
+                                newSubject.setLectureType(value);
+                                break;
+                            }
+                            case 6: { // 학수번호
+                                newSubject.setCourseNumber(value);
+                                break;
+                            }
+                            case 7: { // 교과목국문명
+                                newSubject.setLectureName(value);
+                                break;
+                            }
+                            case 9: { // 학점
+                                newSubject.setCredit(value);
+                                break;
+                            }
+                            default:
+                                break;
+                        }
+                    }
+                    System.out.println();
+                    list.add(newSubject);
+                }
+            }
+            file.close(); // 파일 닫기
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
